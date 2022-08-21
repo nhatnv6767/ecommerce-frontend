fff4a70a3370cd775283fc259cdef26bb56ded44
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 23:47:13 +0900

    #13 Ant Design Icons 7.1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import "antd/dist/antd.min.css"
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {
     BrowserRouter as Router,
 } from "react-router-dom";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
