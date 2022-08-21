cb1636d2ed0933be76a5aee3e3574200146f5021
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 22:20:35 +0900

    #12 Navigate 12.6

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,35 +1,78 @@
 import React, {useState} from 'react';
+import {AppstoreOutlined, MailOutlined, SettingOutlined} from '@ant-design/icons';
 import {Menu} from 'antd';
-import {MailOutlined, SettingOutlined} from '@ant-design/icons';
 
 const {SubMenu} = Menu;
 
+const items = [
+    {
+        label: 'Navigation One',
+        key: 'mail',
+        icon: <MailOutlined/>,
+    },
+    {
+        label: 'Navigation Two',
+        key: 'app',
+        icon: <AppstoreOutlined/>,
+        disabled: true,
+    },
+    {
+        label: 'Navigation Three - Submenu',
+        key: 'SubMenu',
+        icon: <SettingOutlined/>,
+        children: [
+            {
+                type: 'group',
+                label: 'Item 1',
+                children: [
+                    {
+                        label: 'Option 1',
+                        key: 'setting:1',
+                    },
+                    {
+                        label: 'Option 2',
+                        key: 'setting:2',
+                    },
+                ],
+            },
+            {
+                type: 'group',
+                label: 'Item 2',
+                children: [
+                    {
+                        label: 'Option 3',
+                        key: 'setting:3',
+                    },
+                    {
+                        label: 'Option 4',
+                        key: 'setting:4',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        label: (
+            <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                Navigation Four - Link
+            </a>
+        ),
+        key: 'alipay',
+    },
+];
+
 const Header = () => {
 
-    const [current, setCurrent] = useState("")
 
-    const handleClick = () => {
+    const [current, setCurrent] = useState("mail")
 
+    const handleClick = (e) => {
+        console.log('click ', e);
+        setCurrent(e.key);
     }
 
     return (
-        <Menu
-            mode="horizontal"
-            defaultSelectedKeys={[current]}
-            onClick={handleClick}
-        >
-            <Menu.Item key="mail">
-                Home
-            </Menu.Item>
-            <SubMenu key="SubMenu" title="Register">
-                <Menu.Item key="setting:1">
-                    Option 1
-                </Menu.Item>
-                <Menu.Item key="setting:2">
-                    Option 2
-                </Menu.Item>
-            </SubMenu>
-        </Menu>
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" items={items}/>
     );
 };
 
