ef200abe3d3befb1bcac5c27b48fa437610b2c71
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-22 01:00:06 +0900

    #16 Firebase Setup 3

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+import {initializeApp} from "firebase/app";
+import * as firebase from "firebase"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
