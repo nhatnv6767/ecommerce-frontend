72d295dc92709b3fea8073582ecb41158d06d348
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 22:22:32 +0900

    #12 Navigate 13

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
-
+import "antd/dist/antd.css"
 
 const App = () => {
     return (
