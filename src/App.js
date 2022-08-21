8a6678173d41e6fb7bff60fa3d6c168e7f3c1a13
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 21:02:16 +0900

    #11 Routes and Pages 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+
+
 const App = () => {
     return (
-        <div>
-            <p>React App</p>
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login/>} exact/>
+                <Route path="/register" element={<Register/>} exact/>
+                <Route path="" element={<Home/>} exact/>
+            </Routes>
+        </Router>
     );
 }
 
