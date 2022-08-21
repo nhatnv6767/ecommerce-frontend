ddb6e9756da9adddc0f153736ba7be4aebe9d0d8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 23:28:47 +0900

    #13 Ant Design Icons 5.2

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ import {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <React.StrictMode>
-    <Router>
-        <App/>
-    </Router>
-    // </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <App/>
+        </Router>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
