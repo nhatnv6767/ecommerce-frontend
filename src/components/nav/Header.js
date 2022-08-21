814790c80cebc79e913599ee82985d6e3fd24dfa
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 21:33:00 +0900

    #12 Navigate 7

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {AppstoreOutlined, MailOutlined, SettingOutlined} from '@ant-design/icons';
 import type {MenuProps} from 'antd';
 import {Menu} from 'antd';
@@ -6,10 +6,40 @@ import {Menu} from 'antd';
 const {SubMenu} = Menu;
 
 const Header = () => {
+
+    const [current, setCurrent] = useState("")
+
+    const handleClick = () => {
+
+    }
+
     return (
-        <div>
-            Header
-        </div>
+        <Menu
+            mode="horizontal"
+            selectedKeys={[current]}
+            defaultSelectedKeys={['mail']}
+            onClick={handleClick}
+        >
+            <Menu.Item key="mail" icon={<MailOutlined/>}>
+                Navigation One
+            </Menu.Item>
+            <Menu.SubMenu key="SubMenu" title="Navigation Two - Submenu" icon={<SettingOutlined/>}>
+                <Menu.Item key="two" icon={<AppstoreOutlined/>}>
+                    Navigation Two
+                </Menu.Item>
+                <Menu.Item key="three" icon={<AppstoreOutlined/>}>
+                    Navigation Three
+                </Menu.Item>
+                <Menu.ItemGroup title="Item Group">
+                    <Menu.Item key="four" icon={<AppstoreOutlined/>}>
+                        Navigation Four
+                    </Menu.Item>
+                    <Menu.Item key="five" icon={<AppstoreOutlined/>}>
+                        Navigation Five
+                    </Menu.Item>
+                </Menu.ItemGroup>
+            </Menu.SubMenu>
+        </Menu>
     );
 };
 
