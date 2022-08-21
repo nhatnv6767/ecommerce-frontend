e964698d8c697bc2e713730da76802c550a91dc1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 21:23:48 +0900

    #12 Navigate 5

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {AppstoreOutlined, MailOutlined, SettingOutlined} from '@ant-design/icons';
+import type {MenuProps} from 'antd';
+import {Menu} from 'antd';
+
+const {SubMenu} = Menu;
 
 const Header = () => {
     return (
