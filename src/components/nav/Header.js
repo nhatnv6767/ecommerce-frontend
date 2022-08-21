3baa12021029062128466f5656352c18414006b6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 22:24:07 +0900

    #12 Navigate 14

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -6,24 +6,18 @@ const {SubMenu} = Menu;
 
 const items = [
     {
-        label: 'Navigation One',
+        label: 'Home',
         key: 'mail',
         icon: <MailOutlined/>,
     },
     {
-        label: 'Navigation Two',
-        key: 'app',
-        icon: <AppstoreOutlined/>,
-        disabled: true,
-    },
-    {
-        label: 'Navigation Three - Submenu',
+        label: 'Register',
         key: 'SubMenu',
         icon: <SettingOutlined/>,
         children: [
             {
                 type: 'group',
-                label: 'Item 1',
+                label: '',
                 children: [
                     {
                         label: 'Option 1',
@@ -35,20 +29,7 @@ const items = [
                     },
                 ],
             },
-            {
-                type: 'group',
-                label: 'Item 2',
-                children: [
-                    {
-                        label: 'Option 3',
-                        key: 'setting:3',
-                    },
-                    {
-                        label: 'Option 4',
-                        key: 'setting:4',
-                    },
-                ],
-            },
+
         ],
     }
 ];
