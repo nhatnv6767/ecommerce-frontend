572c414fe048938251d47e67479434c806f4b137
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 23:52:34 +0900

    #13 Ant Design Icons 8

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
     BrowserRouter as Router,
 } from "react-router-dom";
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
