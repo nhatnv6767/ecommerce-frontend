39a6321d608bf8b7a66e5c59dd54784a1df248fa
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 21:52:42 +0900

    #12 Navigate 12.2

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {Menu} from 'antd';
-import {MailOutlined, AppstoreOutlined, SettingOutlined} from '@ant-design/icons';
+import {MailOutlined, SettingOutlined} from '@ant-design/icons';
 
 const {SubMenu} = Menu;
 
@@ -18,18 +18,16 @@ const Header = () => {
             defaultSelectedKeys={[current]}
             onClick={handleClick}
         >
-            <Menu.Item key="mail" icon={<MailOutlined/>}>
+            <Menu.Item key="mail">
                 Home
             </Menu.Item>
-            <SubMenu key="SubMenu" title="Register" icon={<SettingOutlined/>}>
-
+            <SubMenu key="SubMenu" title="Register">
                 <Menu.Item key="setting:1">
                     Option 1
                 </Menu.Item>
                 <Menu.Item key="setting:2">
                     Option 2
                 </Menu.Item>
-
             </SubMenu>
         </Menu>
     );
