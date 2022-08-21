4616a5bd5a04dcf9a07ff62711c9e8d578232ef6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-21 21:16:19 +0900

    #12 Navigate 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import {
-    BrowserRouter as Router,
     Routes,
     Route,
-    Navigate,
 } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
