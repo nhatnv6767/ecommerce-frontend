978f9163084dff791cc9bfa058641eb33068cd36
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-22 21:28:38 +0900

    #17 Register form 2

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Register = () => {
+    const registerForm = () => <p>register form</p>
     return (
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <h4>Register</h4>
+                    {registerForm()}
                 </div>
             </div>
         </div>
