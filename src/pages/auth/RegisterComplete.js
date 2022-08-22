a918bbdaabb8f8aaf9314def4f3d7afc27830cc0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-22 22:50:18 +0900

    #20 Complete Registration Page 3

--- src/pages/auth/RegisterComplete.js
+++ src/pages/auth/RegisterComplete.js
@@ -4,36 +4,20 @@ import {getAuth, sendSignInLinkToEmail} from "firebase/auth";
 import {app} from "../../firebase"
 import {toast} from "react-toastify"
 
-const RegisterComplete = (props) => {
+const RegisterComplete = ({history}) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    // props.history
+
 
     const handleSubmit = async (e) => {
         // dont reload page
         e.preventDefault()
-        const auth = getAuth(app);
-        const config = {
-            url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
-            handleCodeInApp: true,
-        }
 
-        await sendSignInLinkToEmail(auth, email, config).then(() => {
-            toast.success(`Email is sent to ${email}. Click the link to complete registration.`)
-            // save user email in local storage
-            window.localStorage.setItem("emailForRegistration", email)
-            // clear state
-            setEmail("")
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ...
-        });
 
     }
 
-    const registerForm = () =>
+    const completeRegistrationForm = () =>
         <form onSubmit={handleSubmit}>
             <input
                 type="email"
