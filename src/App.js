3c034e6ec31f1c2d59f2fa5b50345088155daca9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-23 00:37:41 +0900

    #23 User in Redux State 1

--- src/App.js
+++ src/App.js
@@ -10,8 +10,35 @@ import Home from "./pages/Home";
 import Header from "./components/nav/Header";
 import RegisterComplete from "./pages/auth/RegisterComplete"
 
+import {getAuth} from "firebase/auth";
+import {app} from "./firebase"
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
+import async from "async";
+
 
 const App = () => {
+    const auth = getAuth(app);
+    const dispatch = useDispatch()
+
+    // to check firebase auth state
+    useEffect(() => {
+        const unsubcribe = auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                const idTokenResult = await user.getIdTokenResult()
+                console.log("User (App.js): ", user)
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: {
+                        name: "Noone",
+                    }
+                })
+            }
+        })
+        // cleanup
+        return () => unsubcribe()
+    }, [])
+
     return (
         <>
             <Header/>
