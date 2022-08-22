d97e9d81e7d1e9af041bcd84838fa9d9ec69af70
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-23 00:14:49 +0900

    #22 Setup Redux 3

--- src/index.js
+++ src/index.js
@@ -12,12 +12,17 @@ import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 
+// store
+const store = createStore(rootReducer, composeWithDevTools())
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <React.StrictMode>
-    <Router>
-        <App/>
-    </Router>
+    <Provider>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
     // </React.StrictMode>
 );
 
