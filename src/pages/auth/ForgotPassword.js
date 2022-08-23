1319bfd0da9140a2462ae934fe29377b7e4da9a1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-24 01:09:38 +0900

    #29 Forgot password 5

--- src/pages/auth/ForgotPassword.js
+++ src/pages/auth/ForgotPassword.js
@@ -10,9 +10,31 @@ const ForgotPassword = ({auth}) => {
     const [email, setEmail] = useState("")
     const [loading, setLoading] = useState(false)
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="container col-md-6 offset-md-3 p-5">
-            form to get user email
+            {
+                loading
+                    ? (<h4 className="text-danger">Loading</h4>)
+                    : (<h4>Forgot Password</h4>)
+            }
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    className="form-control"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Type your email"
+                    autoFocus
+                />
+                <br/>
+                <button className="btn btn-raised" disabled={!email}>
+                    Submit
+                </button>
+            </form>
         </div>
     )
 }
