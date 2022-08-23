43bebcb3bddaa9f644998b89fd28a1951f90834d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-23 22:26:50 +0900

    #27 Login with google 2

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 import {signInWithEmailAndPassword} from "firebase/auth";
 import {toast} from "react-toastify"
 import {Button} from "antd";
-import {MailOutlined} from '@ant-design/icons';
+import {GoogleOutlined, MailOutlined} from '@ant-design/icons';
 import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
 
@@ -41,49 +41,70 @@ const Login = ({auth}) => {
         }
     }
 
+    const googleLogin = () => {
+
+    }
+
     const loginForm = () =>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <input
-                    type="email"
-                    className="form-control"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    placeholder="Your email"
-                    autoFocus
-                />
-            </div>
-            <div className="form-group">
-                <input
-                    type="password"
-                    className="form-control"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    placeholder="Your password"
-                />
-            </div>
-            <br/>
-            <Button
-                onClick={handleSubmit}
-                type="primary"
-                className="mb-3"
-                block
-                shape="round"
-                icon={<MailOutlined/>}
-                size="large"
-                disabled={!email || password.length < 6}
-            >
-                Login with Email/Password
-            </Button>
-        </form>
+        (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        className="form-control"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        placeholder="Your email"
+                        autoFocus
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        className="form-control"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        placeholder="Your password"
+                    />
+                </div>
+                <br/>
+                <Button
+                    onClick={handleSubmit}
+                    type="primary"
+                    className="mb-3"
+                    block
+                    shape="round"
+                    icon={<MailOutlined/>}
+                    size="large"
+                    disabled={!email || password.length < 6}
+                >
+                    Login with Email/Password
+                </Button>
+            </form>
+        )
+
     return (
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     {
-                        loading ? <h4>Login</h4> : <h4>Loading..</h4>
+                        loading
+                            ? (<h4>Login</h4>)
+                            : (<h4 className="text-danger">Loading..</h4>)
                     }
                     {loginForm()}
+
+                    <Button
+                        onClick={googleLogin}
+                        type="danger"
+                        className="mb-3"
+                        block
+                        shape="round"
+                        icon={<GoogleOutlined/>}
+                        size="large"
+                    >
+                        Login with Google
+                    </Button>
                 </div>
             </div>
         </div>
