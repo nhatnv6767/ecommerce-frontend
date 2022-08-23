a297f8166a4291eea68b125f274a5eceb2ddecea
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-24 01:02:14 +0900

    #29 Forgot password 3

--- src/pages/auth/ForgotPassword.js
+++ src/pages/auth/ForgotPassword.js
@@ -3,3 +3,18 @@ import React, {useState} from 'react';
 import {signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
 import {toast} from "react-toastify"
 import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+
+const ForgotPassword = ({auth}) => {
+    const history = useNavigate()
+    const [email, setEmail] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <div className="container col-md-6 offset-md-3 p-5">
+            form to get user email
+        </div>
+    )
+}
+
+export default ForgotPassword;
