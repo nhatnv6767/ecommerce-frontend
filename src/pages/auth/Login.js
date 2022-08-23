adedd5f18b4254c3a35928997387ae440d773b87
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-23 21:49:04 +0900

    #25 Login page 1

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+// import {auth} from "../../firebase"
+import {getAuth, sendSignInLinkToEmail} from "firebase/auth";
+import {app} from "../../firebase"
+import {toast} from "react-toastify"
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = async (e) => {
+        // dont reload page
+        e.preventDefault()
+        const auth = getAuth(app);
+        const config = {
+            url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+            handleCodeInApp: true,
+        }
+
+        await sendSignInLinkToEmail(auth, email, config).then(() => {
+            toast.success(`Email is sent to ${email}. Click the link to complete registration.`)
+            // save user email in local storage
+            window.localStorage.setItem("emailForRegistration", email)
+            // clear state
+            setEmail("")
+        }).catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+        });
+
+    }
+
+    const registerForm = () =>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Your email"
+                autoFocus
+            />
+            <br/>
+            <button type="submit" className="btn btn-raised">
+                Register
+            </button>
+        </form>
     return (
-        <div>
-            <p>Login</p>
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4>Register</h4>
+                    {registerForm()}
+                </div>
+            </div>
         </div>
     );
 };
