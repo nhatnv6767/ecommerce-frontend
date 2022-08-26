0f876e6df57e56eaf84b185cfbbe637dbec06d54
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-26 23:09:40 +0900

    #47 Protected User Route 3

---
+++ src/components/routes/UserRoute.js
@@ -0,0 +1,15 @@
+import {
+    Link,
+    Route,
+} from "react-router-dom";
+import {useSelector} from "react-redux";
+
+const UserRoute = ({children, ...rest}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default UserRoute;
