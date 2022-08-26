40eb88d5a98f179d39e5519905b0ad258e339822
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-27 00:24:23 +0900

    #48 Redirect Countdown 2

--- src/components/routes/LoadingToRedirect.js
+++ src/components/routes/LoadingToRedirect.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 
 const LoadingToRedirect = () => {
+    const [count, setCount] = useState(5)
+    let history = useNavigate()
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((currentCount) => --currentCount)
+        })
+        // redirect once count is equal to 0
+
+        count === 0 && history("/")
+
+        // cleanup
+        return () => clearInterval(interval)
+    }, [count])
+
     return (
         <div>
 
