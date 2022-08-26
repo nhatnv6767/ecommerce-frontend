b7d764ff82f104c534bdac97d0b9f80fbb358ac2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-26 21:49:43 +0900

    #44 API User response 3

--- src/pages/auth/RegisterComplete.js
+++ src/pages/auth/RegisterComplete.js
@@ -3,6 +3,16 @@ import React, {useEffect, useState} from 'react';
 import {signInWithEmailLink, updatePassword} from "firebase/auth";
 import {toast} from "react-toastify"
 import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import axios from "axios";
+
+const createOrUpdateUser = async (authtoken) => {
+    return await axios.post(`${process.env.REACT_APP_API}/create-or-update-user`, {}, {
+        headers: {
+            authtoken,
+        }
+    })
+}
 
 const RegisterComplete = ({auth}) => {
 
@@ -10,12 +20,13 @@ const RegisterComplete = ({auth}) => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const {user} = useSelector(state => ({...state}))
 
     useEffect(() => {
         setEmail(window.localStorage.getItem("emailForRegistration"))
     }, [])
 
-
+    let dispatch = useDispatch()
     const handleSubmit = async (e) => {
         // dont reload page
         e.preventDefault()
@@ -42,6 +53,25 @@ const RegisterComplete = ({auth}) => {
                 const idTokenResult = await user.getIdTokenResult()
                 // redux store
                 console.log("user: ", user, "idTokenResult: ", idTokenResult)
+
+                await createOrUpdateUser((idTokenResult.token))
+                    .then(
+                        res => {
+                            dispatch({
+                                type: "LOGGED_IN_USER",
+                                payload: {
+                                    name: res.data.name,
+                                    email: res.data.email,
+                                    token: idTokenResult.token,
+                                    role: res.data.role,
+                                    _id: res.data._id,
+                                }
+                            })
+                        }
+                    )
+                    .catch()
+
+
                 // redirect
                 history("/")
             }
