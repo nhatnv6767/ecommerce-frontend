79bc5a18af3b6f04a67a0fd589d36f4a564f6d4d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-26 21:43:30 +0900

    #44 API User response 2

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -71,13 +71,22 @@ const Login = ({auth}) => {
             .then(async (result) => {
                 const {user} = result;
                 const idTokenResult = await user.getIdTokenResult()
-                dispatch({
-                    type: "LOGGED_IN_USER",
-                    payload: {
-                        email: user.email,
-                        token: idTokenResult.token,
-                    }
-                })
+                await createOrUpdateUser((idTokenResult.token))
+                    .then(
+                        res => {
+                            dispatch({
+                                type: "LOGGED_IN_USER",
+                                payload: {
+                                    name: res.data.name,
+                                    email: res.data.email,
+                                    token: idTokenResult.token,
+                                    role: res.data.role,
+                                    _id: res.data._id,
+                                }
+                            })
+                        }
+                    )
+                    .catch()
                 history('/')
             }).catch(err => {
                 console.log(err)
