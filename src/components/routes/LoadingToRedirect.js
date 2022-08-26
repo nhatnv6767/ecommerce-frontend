d47e0e225f60529e3f28e9aaa82d8ccf040c151c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-26 23:42:20 +0900

    #48 Redirect Countdown 1

---
+++ src/components/routes/LoadingToRedirect.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+
+const LoadingToRedirect = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LoadingToRedirect;
