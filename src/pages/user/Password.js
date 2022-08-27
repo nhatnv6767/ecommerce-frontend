4edfdcfb51ca8b34dbbdf25aef0353a95a77ff49
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-27 22:27:00 +0900

    #50 Password update 3

--- src/pages/user/Password.js
+++ src/pages/user/Password.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UserNav from "../../components/nav/UserNav";
+import {toast} from "react-toastify";
 
 const Password = ({auth}) => {
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const passwordUpdateForm = () =>
+        <form>from</form>
+
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-md-2">
                     <UserNav/>
                 </div>
-                <div className="col">User password page</div>
+                <div className="col">
+                    <h4>Password Update</h4>
+                    {passwordUpdateForm()}
+                </div>
             </div>
         </div>
     );
