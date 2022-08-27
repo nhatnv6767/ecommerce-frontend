93f9acab5dd6539c83a8934f0872c26d385daa4a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-27 20:52:36 +0900

    #49 Sidebar Nav for User 2

--- src/components/nav/UserNav.js
+++ src/components/nav/UserNav.js
@@ -4,7 +4,17 @@ import {Link} from "react-router-dom"
 const UserNav = () => {
     return (
         <nav>
-
+            <ul className="nav flex-column">
+                <li className="nav-item">
+                    <Link to="user/history">History</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="user/password">Password</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="user/wishlist">Wishlist</Link>
+                </li>
+            </ul>
         </nav>
     );
 };
