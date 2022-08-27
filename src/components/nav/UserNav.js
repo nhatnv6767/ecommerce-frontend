991f6e1fe39d44710d3800add7080f3e8c9fc25e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-08-27 22:12:31 +0900

    #49 Sidebar Nav for User 5

--- src/components/nav/UserNav.js
+++ src/components/nav/UserNav.js
@@ -6,13 +6,13 @@ const UserNav = () => {
         <nav>
             <ul className="nav flex-column">
                 <li className="nav-item">
-                    <Link to="/user/history" className="nav-link">History</Link>
+                    <Link to="/user/history" className="nav-link text-uppercase">History</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/user/password" className="nav-link">Password</Link>
+                    <Link to="/user/password" className="nav-link text-uppercase">Password</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/user/wishlist" className="nav-link">Wishlist</Link>
+                    <Link to="/user/wishlist" className="nav-link text-uppercase">Wishlist</Link>
                 </li>
             </ul>
         </nav>
